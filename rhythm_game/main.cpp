8409f3885b03577189e8206c945d2ad1a008d9da
Miquel Jayson de Leeuw <jaysondlw@gmail.com>
2025-10-09 02:07:04 +0200

    linked goloud with example

--- rhythm_game/main.cpp
+++ rhythm_game/main.cpp
@@ -1,10 +1,35 @@
-#include "soloud.h"
 #include "stdint.h"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+#include "soloud.h"
+#include "soloud_wav.h"
+#include "soloud_thread.h"
 
-    std::cout << "HELLO WORLD" << std::endl;
+int main(int argc, char* argv[]) {
+    std::cout << "Running..." << std::endl;
+
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_wav_file>" << std::endl;
+        return 1;
+    }
+
+    const char* filename = argv[1];
+
+    SoLoud::Soloud gSoloud;
+    SoLoud::Wav gWave;
+
+    gSoloud.init();
+
+    gWave.load(filename);
+
+    gSoloud.play(gWave);
+
+	while (gSoloud.getActiveVoiceCount() > 0)
+	{
+		SoLoud::Thread::sleep(100);
+	}
+
+    gSoloud.deinit();
     
     return 0;
 }
